3fddaac571de8529800d457ba8717ec08e05ab38
udbhav3101 <udbhav3101@gmail.com>
2022-06-08 14:32:50 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Home/Home';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import './index.css';
 
 function App() {
 
   const title = "Welcome to my Blog";
 
   return (
-    <div className="App">
-    <Navbar/>
-    <Home/>
-    </div>
+    <>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <div className="content">
+        <Routes>
+          <Route exact path='/' element={<><Home/></>}/>
+        </Routes>
+        </div>
+      </div>
+    </Router>
+    </>
   );
 }
 
