3919cef09f717af0bf0670541b1c3ca9c21ea18c
udbhav3101 <udbhav3101@gmail.com>
2022-06-09 13:01:58 +0530

    Changed Blogs URL

--- src/useFetch.js
+++ src/useFetch.js
@@ -16,7 +16,7 @@ const useFetch = (url) => {
         setTimeout(() => {
             fetch(url, { signal: abortCont.signal })
                 .then(res =>{
-                    // console.log(res);
+                    console.log(res);
 
                     if(!res.ok){
                     throw Error('Could not fetch data for the response')
@@ -30,11 +30,10 @@ const useFetch = (url) => {
                 })
                 .catch(err => {
                     if(err.name === 'AbortError'){
-                        console.log('Fetch Aborted')
-                    }else{
-                        setLoading(false);
-                        setErrorMessage(err.message);
+                        console.log(err);
                     }
+                    setLoading(false);
+                    setErrorMessage(err.message);
                 })
         },1000);
 
