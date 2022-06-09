45ac5db0c2a37b39586b3555fb23d759a2c27d2a
udbhav3101 <udbhav3101@gmail.com>
2022-06-09 14:46:03 +0530

    Changed Commit

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const middlewares = jsonServer.defaults();
 
 
 const PORT = process.env.PORT || 8000
-server.use(middlewares)
+// server.use(middlewares)
 
-server.use(jsonServer.rewriter({
-    '/api/*':'/$1',
-}))
+// server.use(jsonServer.rewriter({
+//     '/api/*':'/$1',
+// }))
 
-server.use(router);
-
+// server.use(router);
+app.use('./db', middlewares, router);
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
