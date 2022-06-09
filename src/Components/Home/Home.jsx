ad200b015caf919db536bed4f46d0ca7d4f6c3a2
udbhav3101 <udbhav3101@gmail.com>
2022-06-09 10:44:32 +0530

    Minor Change

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,7 +6,7 @@ import './Home.css';
 
 const Home = () => {
 
-  const {data: blogs, loading, errorMessage} = useFetch(' https://reactblog-uv.herokuapp.com/')
+  const {data: blogs, loading, errorMessage} = useFetch('https://reactblog-uv.herokuapp.com/')
       
     
   return (
