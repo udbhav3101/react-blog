6a0eeb5e2d88de42f7a6767cea0315cf94a7b797
udbhav3101 <udbhav3101@gmail.com>
2023-03-22 12:36:50 +0530

    added express to deploy static folder

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const jsonServer = require('json-server');
-
+const express = require('express');
+const path = require('path');
 const server = jsonServer.create();
 
 const router = jsonServer.router('./db.json');
@@ -19,7 +20,10 @@ server.use(
 );
 
 server.use(router);
-
+server.use(express.static(path.join(__dirname, 'build')));
+server.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 server.listen(PORT, () => {
   console.log('Server is running');
 });
